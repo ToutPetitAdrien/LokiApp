2c2fc05eeb56dd591d845c01301521e29905a2d3
Mumu Des Bois <mula.adri@gmail.com>
2016-08-18 22:22:57 +0200

    Account

---
+++ server/accounts.js
@@ -0,0 +1,9 @@
+ServiceConfiguration.configurations.remove({
+    service: 'facebook'
+});
+
+ServiceConfiguration.configurations.insert({
+    service: 'facebook',
+    appId: '986317348133079',
+    secret: 'b9bb6c9b04ddebb9116bc36df4be73b1'
+})
