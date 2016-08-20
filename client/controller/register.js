70dbdaaed91abdf0ce46d67780a949e410ad7678
Mumu Des Bois <mula.adri@gmail.com>
2016-08-20 17:31:38 +0200

    End FB profile

--- client/controller/register.js
+++ client/controller/register.js
@@ -1,3 +1,19 @@
 Template.register.events({
-    'click'
+    'click .facebook-login': function(event){
+        Meteor.loginWithFacebook({loginStyle : 'popup'}, function(err){
+            if(err){
+                throw new Meteor.error("Connexion à Facebook a échoué");
+                console.log(err.reason);
+            }
+        })
+    },
+    'click .facebook-logout': function(event){
+        Meteor.logout(function(err){
+            if(err){
+                throw new Meteor.error("Logout failed");
+                console.log(err.reason);
+            }
+        })
+    }
+
 })
--- server/accounts.js
+++ server/accounts.js
@@ -1,3 +1,4 @@
+//configuration connection avec facebook
 ServiceConfiguration.configurations.remove({
     service: 'facebook'
 });
@@ -6,4 +7,18 @@ ServiceConfiguration.configurations.insert({
     service: 'facebook',
     appId: '986317348133079',
     secret: 'b9bb6c9b04ddebb9116bc36df4be73b1'
+})
+
+Accounts.onCreateUser(function(options,user){
+    options.profile.email = user.services.facebook.email;
+    options.profile.family_name = user.services.facebook.last_name;
+    options.profile.first_name = user.services.facebook.first_name;
+    options.profile.picture = "http://graph.facebook.com/" + user.services.facebook.id + "/picture/?type=large";
+    options.profile.gender = user.services.facebook.gender;
+    options.profile.link = user.services.facebook.link;
+    options.profile.service = "facebook";
+
+    user.profile = options.profile
+
+    return user
 })
