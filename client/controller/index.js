596320523b8d779e45349be35adc5dca51452539
Mumu Des Bois <mula.adri@gmail.com>
2016-08-24 17:42:51 +0200

    Event Actions

--- client/controller/index.js
+++ client/controller/index.js
@@ -7,8 +7,25 @@ Template.index.events({
             }
         })
     },
-    'click .button-popo': function(event){
-        Meteor.call('addActions', 'Promenade')
-        console.log('ok !');
+    'click .actions_popo': function(event){
+        Meteor.call('addActions', 'popo')
+        console.log('ok popo');
+    },
+    'click .actions_promenade': function(event){
+        Meteor.call('addActions', 'promenade')
+        console.log('ok promenade');
+    },
+    'click .actions_friandise': function(event){
+        Meteor.call('addActions', 'friandise')
+        console.log('ok friandise');
+    },
+    'click .actions_repas': function(event){
+        Meteor.call('addActions', 'repas')
+        console.log('ok repas');
+    },
+    'click .actions_douche': function(event){
+        Meteor.call('addActions', 'douche')
+        console.log('ok douche');
     }
+
 })
